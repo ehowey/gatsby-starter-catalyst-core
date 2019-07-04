f9ea8994408a9401a41d74856975ba6f6313f34b
Eric Howey <ehowey@gmail.com>
2019-07-03 21:32:03 -0600

    Removing Experimental Tags, removing es6 transpile

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,24 +18,19 @@ module.exports = {
           }
             ]
   },
-    __experimentalThemes: [
-        'gatsby-theme-catalyst-core'
-    ],
-    plugins: [ 
+    plugins: [
+      'gatsby-theme-catalyst-core', 
       {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-starter-catalyst-basic`,
+        short_name: `catalyst`,
         start_url: `/`,
-        background_color: `#000000`,
+        background_color: `#ffffff`,
         theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
-      },
+      }
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     ]
 }
