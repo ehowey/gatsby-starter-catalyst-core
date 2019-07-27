7cf8706e2f8781b9695f545fb5555e2db9ae03be
Eric Howey <ehowey@gmail.com>
2019-07-27 06:32:05 -0600

    Logo and formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,38 +3,38 @@ module.exports = {
     title: `Catalyst Core`,
     description: `Speed up your Gatsby development workflow. Designed as an opinionated and high speed starter.`,
     author: `Eric Howey`,
-    menuLinks:[
-          {
-            name:'Page 1',
-            link:'/page-1'
-          },
-          {
-            name:'Page 2',
-            link:'/page-2'
-          },
-          {
-            name:'Page 3',
-            link:'/page-3'
-          }
-            ],
-    socialLinks:[
+    menuLinks: [
       {
-        name:'Email',
-        url:'eric@erichowey.dev'
+        name: `Page 1`,
+        link: `/page-1`
       },
       {
-        name:'Github',
-        url:'https://www.github.com/ehowey'
+        name: `Page 2`,
+        link: `/page-2`
       },
       {
-        name:'Twitter',
-        url:'https://www.twitter.com/erchwy'
+        name: `Page 3`,
+        link: `/page-3`
+      }
+    ],
+    socialLinks: [
+      {
+        name: `Email`,
+        url: `eric@erichowey.dev`
+      },
+      {
+        name: `Github`,
+        url: `https://www.github.com/ehowey`
+      },
+      {
+        name: `Twitter`,
+        url: `https://www.twitter.com/erchwy`
       }
     ]
   },
-    plugins: [
-      'gatsby-theme-catalyst-core', 
-      {
+  plugins: [
+    `gatsby-theme-catalyst-core`,
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-catalyst-basic`,
@@ -43,8 +43,8 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png` // This path is relative to the root of the site.
       }
     }
-    ]
-}
+  ]
+};
